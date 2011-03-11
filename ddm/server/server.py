6f2057f3d675a9af1b922d751a11d177e727b5c3
Sadjad Fouladi <sfouladi@gmail.com>
2011-03-11 19:22:53 +0330

    Server commands created.

--- ddm/server/server.py
+++ ddm/server/server.py
@@ -1,7 +1,53 @@
-from twisted.internet import protocol, reactor
+from twisted.internet import protocol
+from twisted.internet import reactor
 from twisted.protocols import basic
 
+from server_model import *
+
 import simplejson
+
+def create_error(message):
+    response = {}
+    response['type'] = "error"
+    response['message'] = message
+    return simplejson.dumps(response)
+
+def process_command(command):
+    response = {}
+    
+    if 'command' not in command:
+        return create_error("Command not found.")
+    
+    if command['command'] == 'list':
+        files = DownloadRequest.select().where(state=0)
+        response['request_id'] = command['request_id']
+        response['files'] = []
+        
+        for f in files:
+            response['files'].append((f.url, f.size, f.chunks))
+            
+        return simplejson.dumps(response)
+        
+    elif command['command'] == 'fileinfo':
+        pass
+    elif command['command'] == 'getchunk':
+        pass
+    else:
+        return create_error("Unknown command.")
+
+def process_request(req):
+    request =simplejson.loads(req);
+    response = {}
+    
+    if 'request_id' not in req:
+        response['request_id'] = None
+    
+    if 'type' not in req:
+        return create_error("Invalid request.")
+    
+    if request['type'] == 'command':
+        return process_command(request)
+        
 
 class DDMProtocol(basic.LineReceiver):
     def lineReceived(self, data):
@@ -10,5 +56,5 @@ class DDMProtocol(basic.LineReceiver):
 class DDMServerFactory(protocol.ServerFactory):
     protocol = DDMProtocol
     
-reactor.listenTCP(22999, DDMServerFactory())
-reactor.run()
+reactor.listenTCP(22999, DDMServerFactory()) #@UndefinedVariable
+reactor.run() #@UndefinedVariable
--- ddm/server/server_model.py
+++ ddm/server/server_model.py
@@ -14,6 +14,13 @@ class DownloadRequest(peewee.Model):
     user = peewee.ForeignKeyField(User)
     url = peewee.CharField()
     chunks_count = peewee.IntegerField()
+    size = peewee.IntegerField()
     state = peewee.IntegerField()
+    
+class FileChunk(peewee.Model):
+    file = peewee.ForeignKeyField(DownloadRequest)
+    index = peewee.IntegerField()
+    chunk_state = peewee.IntegerField()
+    
     
     
